04e9ae6b987cfb73be6204416e0bd7ea978a8e5e
Daniel J Blueman <daniel@quora.org>
2024-11-04 15:25:21 +0800

    Allow multiple recipients

--- mailer.go
+++ mailer.go
@@ -1,7 +1,9 @@
 package ratemail
 
 import (
+   "fmt"
    "regexp"
+   "strings"
    "sync"
    "time"
 
@@ -57,24 +59,27 @@ func NewMailer(host string, port int, user, pass, from string) *Mailer {
 
 func (mailer *Mailer) Check() error {
    s, err := mailer.Dial()
-   if err == nil {
-      s.Close()
+   if err != nil {
+      return fmt.Errorf("Check: %w", err)
    }
-   return err
+
+   s.Close()
+   return nil
 }
 
-func (mailer *Mailer) Send(to, subject, bodyType, body string) error {
+func (mailer *Mailer) Send(to []string, subject, bodyType, body string) error {
    msg := gomail.NewMessage()
    msg.SetHeader("From", mailer.from)
-   msg.SetHeader("To", to)
+   msg.SetHeader("To", to...)
    msg.SetHeader("Subject", subject)
    msg.SetBody(bodyType, body)
 
    return mailer.DialAndSend(msg)
 }
 
-func (mailer *Mailer) SendRate(to, subject, bodyType, body string) error {
-   key := re.ReplaceAllString(to+subject+body, "")
+func (mailer *Mailer) SendRate(to []string, subject, bodyType, body string) error {
+   toAll := strings.Join(to, ",")
+   key := re.ReplaceAllString(toAll+subject+body, "")
 
    lock.Lock()
    defer lock.Unlock()
